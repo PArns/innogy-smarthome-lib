b8fca28d19ff869d89623189cf424deef686ef90
PArns <github@patrick-arns.de>
2017-01-02 20:38:53 +0100

    - Added states - Extended example

--- index.js
+++ index.js
@@ -11,10 +11,55 @@ smartHome.on("stateChanged", function (objectWhichStateHasChanged) {
 });
 
 smartHome.on("initializationComplete", function () {
-    console.log("INIT COMPLETE!");
+    console.log("INITIALIZATION SEQUENCE COMPLETED");
 
-    var testSwitch = smartHome.getCapabilityById("461a98f206db40c0b0e1433d2bb240dd");
-    testSwitch.setState(false);
+    if (smartHome.device && smartHome.device.length) {
+        console.log("LIST OF ALL REGISTERED DEVICES:");
+
+        smartHome.device.forEach(function (aDevice) {
+            console.log("----------------------------------------");
+
+            console.log("ID:", aDevice.id);
+            console.log("Type:", aDevice.type);
+            console.log("Name:", aDevice.getName()); // Helper needed, as name is stored within configuration!
+
+            if (aDevice.Location)
+                console.log("Location:", aDevice.Location.getName()); // Helper needed, as name is stored within configuration!
+
+            console.log("- CAPABILITIES:");
+
+            aDevice.Capabilities.forEach(function (aCapability) {
+                console.log("    ID:", aCapability.id);
+                console.log("    Type:", aCapability.type);
+
+                if (aCapability.Config.length) {
+                    console.log("    - CONFIG:");
+
+                    aCapability.Config.forEach(function (aState) {
+                        console.log("        Name:", aState.name);
+                        console.log("        Type:", aState.type);
+                        console.log("        Access:", aState.access);
+                        console.log("        Value:", aState.value);
+                        console.log("        LastChanged:", aState.lastchanged);
+
+                    });
+                }
+
+                if (aCapability.State.length) {
+                    console.log("    - STATES:");
+
+                    aCapability.State.forEach(function (aState) {
+                        console.log("        Name:", aState.name);
+                        console.log("        Type:", aState.type);
+                        console.log("        Access:", aState.access);
+                        console.log("        Value:", aState.value);
+                        console.log("        LastChanged:", aState.lastchanged);
+
+                    });
+                }
+            });
+        });
+    }
 });
 
 smartHome.init();
