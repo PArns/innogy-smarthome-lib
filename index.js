fb989c08c68487d9a74a7b5370cb8caa3af7bbb5
PArns <github@patrick-arns.de>
2017-01-02 18:20:53 +0100

    - Reintegrated _requestor to be able to call functions within the classes - Added first setState for capabilities (currently only OnOff supported)

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ smartHome.on("stateChanged", function (objectWhichStateHasChanged) {
 
 smartHome.on("initializationComplete", function () {
     console.log("INIT COMPLETE!");
+
+    var testSwitch = smartHome.getCapabilityById("461a98f206db40c0b0e1433d2bb240dd");
+    testSwitch.setState(false);
 });
 
 smartHome.init();
